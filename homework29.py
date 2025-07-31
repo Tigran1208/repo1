851dc12ed66daebc31594887065a9bcdcf3c7dc8
Tigran1208 <tigran.mnatsakanyan12@gmail.com>
2025-08-01 02:14:50 +0400

    Add files via upload

---
+++ homework29.py
@@ -0,0 +1,23 @@
+# class Animal:
+#     def eat(self):
+#         print("Animal is eating")
+#     def sleep(self):
+#         print("Animal is sleeping")
+# # a=Animal()
+# # a.eat()
+# # a.sleep()
+#
+# class Bird(Animal):
+#     def eat(self):
+#         print("Bird is pecking its food")
+#     def fly(self):
+#         print("Bird is flying")
+# class Fish(Animal):
+#     def swim(self):
+#         print("Fish is swimming")
+# b=Bird()
+# b.eat()
+# b.fly()
+# c=Fish()
+# c.swim()
+
